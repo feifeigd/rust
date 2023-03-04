a7132762bdb414c31a13eee64ff4755b2d5e7a1f
feifeigd <feifeigd@21cn.com>
2023-03-04 10:14:46 +0800

    cargo test

--- xtnet/src/lib.rs
+++ xtnet/src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+fn pow(base:i64, exponent:usize)-> i64{
+    let mut res=1;
+    for _ in 0..exponent{
+        res *= base;
+    }
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +18,10 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+    #[test]
+    fn minus_two_raised_three_is_minus_eight(){
+        assert_eq!(pow(-2, 0), 1);
+        assert_eq!(pow(-2, 3), -8);
     }
 }
