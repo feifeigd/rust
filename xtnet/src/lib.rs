2f7be76ac24109db79d65668813a79a8e2f35f6b
feifeigd <feifeigd@21cn.com>
2023-03-04 18:37:12 +0800

    集成测试

--- xtnet/src/lib.rs
+++ xtnet/src/lib.rs
@@ -14,6 +14,10 @@ pub fn greet() {
     println!("Hi from xtnet")
 }
 
+pub fn sum(a: i8, b: i8) -> i8 {
+    a + b
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,5 +31,15 @@ mod tests {
     fn minus_two_raised_three_is_minus_eight() {
         assert_eq!(pow(-2, 0), 1);
         assert_eq!(pow(-2, 3), -8);
+    }
+
+    fn sum_inputs_outputs() -> Vec<((i8, i8), i8)> {
+        vec![((1, 1), 2), ((0, 0), 0), ((2, -2), 0)]
+    }
+    #[test]
+    fn test_sums() {
+        for (input, output) in sum_inputs_outputs() {
+            assert_eq!(sum(input.0, input.1), output);
+        }
     }
 }
---
+++ xtnet/tests/sum.rs
@@ -0,0 +1,5 @@
+use xtnet::sum;
+#[test]
+fn sum_test() {
+    assert_eq!(sum(6, 8), 14);
+}
