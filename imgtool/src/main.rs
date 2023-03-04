5278da9611dccf5eae7c8f0c2db77df75cbeae13
feifeigd <feifeigd@21cn.com>
2023-03-04 17:29:17 +0800

    旋转图形

---
+++ imgtool/src/main.rs
@@ -0,0 +1,11 @@
+use std::path::Path;
+
+
+use std::env;
+fn main() {
+    let image_path = env::args().skip(1).next().unwrap();
+    let path = Path::new(&image_path);
+    let img = image::open(path).unwrap();
+    let rotated = img.rotate90();
+    rotated.save(path).unwrap();
+}
