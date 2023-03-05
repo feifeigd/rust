0b15d2fa8f551ad315436adec4463984e82ffa8b
feifeigd <feifeigd@21cn.com>
2023-03-06 01:22:51 +0800

    Add

--- complex/src/lib.rs
+++ complex/src/lib.rs
@@ -1,4 +1,6 @@
-#[derive(Default)]
+use std::ops::Add;
+
+#[derive(Debug, Default, PartialEq, Clone, Copy)]
 struct Complex<T> {
     re: T,
     im: T,
@@ -10,17 +12,35 @@ impl<T> Complex<T> {
     }
 }
 
+// a + b
+impl<T: Add<T, Output = T>> Add for Complex<T> {
+    type Output = Complex<T>;
+    fn add(self, rhs: Self) -> Self::Output {
+        Complex {
+            re: self.re + rhs.re,
+            im: self.im + rhs.im,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-
     fn complex_basics() {
         let first = Complex::new(3, 5);
         let second: Complex<i32> = Complex::default();
         assert_eq!(first.re, 3);
         assert_eq!(first.im, 5);
         assert!(second.im == second.im);
+    }
+
+    #[test]
+    fn complex_addition() {
+        let a = Complex::new(1, -2);
+        let b = Complex::default();
+        let res = a + b;
+        assert_eq!(res, a); // Debug, Clone,Copy
     }
 }
