38bda83d485ffab5a7673938685061a1c773f5e0
feifeigd <feifeigd@21cn.com>
2023-03-26 11:47:38 +0800

    log4rs

---
+++ my_lib/src/lib.rs
@@ -0,0 +1,24 @@
+
+use log::debug;
+
+pub struct Config;
+impl Config{
+    pub fn load_global_config(){
+        debug!("Configuration files loaded");
+    }
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
